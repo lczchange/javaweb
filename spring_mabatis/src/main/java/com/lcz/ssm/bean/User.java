86c9dc4196362fc99203f71afd8d0e5585a0f600
lczchange <lcz_change@163.com>
2018-10-09 19:25:49 +0800

    测试

---
+++ spring_mabatis/src/main/java/com/lcz/ssm/bean/User.java
@@ -0,0 +1,40 @@
+package com.lcz.ssm.bean;
+
+public class User {
+    private String name;
+    private String password;
+    private String ext;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", ext='" + ext + '\'' +
+                '}';
+    }
+}
--- spring_mabatis/src/main/test/TestJava.java
+++ spring_mabatis/src/main/test/TestJava.java
@@ -1,3 +1,6 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.lcz.ssm.bean.User;
 import org.junit.Test;
 
 public class TestJava {
@@ -5,5 +8,10 @@ public class TestJava {
     public void test1(){
         System.out.println("hello world!");
         System.out.println("第二次提交");
+
+        String json = "{\"name\":\"aa\",\"password\":\"123\"}";
+        User user = JSONObject.parseObject(json, User.class);
+        System.out.println(user.toString());
+
     }
 }
