bf798210256c28e041916d08d4a7f276cb711d3e
lczchange <lcz_change@163.com>
2018-11-09 17:01:25 +0800

    spring 事务练习

--- Generator/src/main/java/com/lcz/ssm/test/GeneratorSqlmap.java
+++ Generator/src/main/java/com/lcz/ssm/test/GeneratorSqlmap.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.File;
+import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GeneratorSqlmap {
-    public void generator() throws Exception{
+    public void generator() throws Exception {
 
         List<String> warnings = new ArrayList<String>();
         boolean overwrite = true;
@@ -26,6 +27,7 @@ public class GeneratorSqlmap {
         myBatisGenerator.generate(null);
 
     }
+
     public static void main(String[] args) throws Exception {
         try {
             GeneratorSqlmap generatorSqlmap = new GeneratorSqlmap();
@@ -37,7 +39,7 @@ public class GeneratorSqlmap {
     }
 
     @Test
-    public void jdbctest(){
+    public void jdbctest() {
         try {
             //注册驱动
             Class.forName("com.mysql.jdbc.Driver");
@@ -49,10 +51,9 @@ public class GeneratorSqlmap {
             //执行操作，获取结果集
             ResultSet resultSet = statement.executeQuery();
             //遍历结果集
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 System.out.println(resultSet.getString("username"));
             }
-
             //释放资源
             resultSet.close();
             statement.close();
@@ -60,8 +61,40 @@ public class GeneratorSqlmap {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
     }
 
+    @Test
+    public void ioTest() {
+        //包装流
+        BufferedReader bis = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            InputStream fis = new FileInputStream(GeneratorSqlmap.class.getClassLoader().getResource("games.txt").getPath());
+            bis = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            String gameId;
+            String gameName;
+            Class.forName("com.mysql.jdbc.Driver");
+            String path = "jdbc:mysql://localhost:3306/hmdb?characterEncoding=utf-8";
+            connection = DriverManager.getConnection(path, "root", "");
+            preparedStatement = connection.prepareStatement("insert into account (name, money) values (?,?)");
+            //读取文件内容
+            while ((line = bis.readLine()) != null) {
+//                line = bis.readLine();
+                gameId = line.substring(line.indexOf(">") + 1, line.lastIndexOf(" |"));
+                gameName = line.substring(line.indexOf("name=>") + 6, line.lastIndexOf("}"));
+                preparedStatement.setString(1, gameName);
+                preparedStatement.setFloat(2, Float.valueOf(gameId));
+            }
+            preparedStatement.close();
+            connection.close();
+            bis.close();
+            fis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+    }
 }
--- spring_tx/src/main/java/com/lcz/spring_tx/test/TransationTest.java
+++ spring_tx/src/main/java/com/lcz/spring_tx/test/TransationTest.java
@@ -1,11 +1,14 @@
 package com.lcz.spring_tx.test;
 
+import com.google.gson.Gson;
 import com.lcz.spring_tx.domain.Acount;
 import com.lcz.spring_tx.service.IAcountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 public class TransationTest {
 
@@ -18,5 +21,14 @@ public class TransationTest {
         Acount acount = acountService.findAcountById(1);
         System.out.println(acount.toString());
 
+    }
+
+    //"[{"channel_id":100},{"channel_id":200}]"
+    @Test
+    public void testjson(){
+        String josn = "[{\"channel_id\":100},{\"channel_id\":200}]";
+        Gson gson = new Gson();
+        Map<String,Integer> map = gson.fromJson(josn, Map.class);
+        System.out.println(map.size());
     }
 }
